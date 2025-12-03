ac4152209d47f55bb64ecddc5658eba541e9b29e
Sabu J <mail@jsabu.com>
2025-12-03 11:59:07 +0400

    Fix: Remove unused RootController that was causing route conflicts

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RootController } from './root.controller';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
